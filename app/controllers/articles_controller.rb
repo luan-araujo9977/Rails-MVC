26899ae5a3f510ee16593c4ae38010e2dc25020b
Luan Araujo <luanaraujo9977@gmail.com>
2022-11-30 16:29:20 -0300

    adicionando filtro por período

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,7 @@ class ArticlesController < ApplicationController
 
     @highlights = Article.includes(:category, :user)
                          .filter_by_category(category)
+                         .filter_by_archives(params[:month_year])
                          .desc_order
                          .first(3)
 
@@ -20,8 +21,11 @@ class ArticlesController < ApplicationController
     @articles = Article.includes(:category, :user)
                        .wihout_highlights(highlights_ids)
                        .filter_by_category(category)
+                       .filter_by_archives(params[:month_year])
                        .desc_order
-                       .page(current_page).per(2)
+                       .page(current_page)
+
+    @archives = Article.group_by_month(:created_at, format: '%B %Y').count
   end
   # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
